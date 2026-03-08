c08651e7aff4fa2a544b9a013dbc6db844e25b39
Vincent Leroy <vincent.leroy@rayference.eu>
2026-03-08 23:30:37 +0100

    Add test cases

--- playgrounds/cases.py
+++ playgrounds/cases.py
@@ -4,6 +4,9 @@ from eradiate.units import unit_registry as ureg
 
 
 def no_atmo(sza: float = 0.0):
+    """
+    Generate an experiment for the "No atmosphere" test case series.
+    """
     return AtmosphereExperiment(
         geometry={
             "type": "plane_parallel",
@@ -24,10 +27,36 @@ def no_atmo(sza: float = 0.0):
 
 
 def single_layer(sza: float = 0.0, phase: str = "isotropic"):
+    """
+    Generate an experiment for the "Single layer" test case series.
+    """
     return AtmosphereExperiment(
         geometry={
             "type": "plane_parallel",
             "toa_altitude": 1.0 * ureg.km,
+        },
+        surface={"type": "lambertian", "reflectance": 0.0},
+        atmosphere={"type": "homogeneous", "phase": {"type": phase}},
+        illumination={"type": "directional", "zenith": sza, "azimuth": 0.0},
+        measures={
+            "type": "mdistant",
+            "construct": "hplane",
+            "azimuth": 0.0,
+            "zeniths": np.arange(-75.0, 76.0, 1.0),
+            "srf": {"type": "delta", "wavelengths": [550.0]},
+        },
+    )
+
+
+def two_layers(sza: float = 0.0):
+    """
+    Generate an experiment for the "Two layers" test case series.
+    """
+    return AtmosphereExperiment(
+        geometry={
+            "type": "plane_parallel",
+            "toa_altitude": 100.0 * ureg.km,
+            "zgrid": np.linspace(0, 100, 11) * ureg.km,
         },
         surface={"type": "lambertian", "reflectance": 0.0},
         atmosphere={"type": "homogeneous", "phase": {"type": phase}},
