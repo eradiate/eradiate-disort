cae339c302c24cdf81e499c31e188d5760c5495e
Vincent Leroy <vincent.leroy@rayference.eu>
2026-06-03 11:42:22 +0200

    Docs: Tidy up configuration, add logo

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,18 @@
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""Sphinx configuration for eradiate-disort documentation."""
 
+import datetime
 from importlib.metadata import version as get_version
 
+# Project information
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = "eradiate-disort"
-copyright = "2025, Vincent Leroy"
-author = "Vincent Leroy"
+copyright = f"2025-{datetime.datetime.now().year}, Rayference"
+author = "The Eradiate Team"
 version = get_version("eradiate-disort")
+release = version
 
-# -- General configuration ---------------------------------------------------
+# General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     # Core extensions
     "sphinx.ext.autodoc",
@@ -27,23 +29,21 @@ extensions = [
     "autodocsumm",
 ]
 
-# -- MyST-NB options ---------------------------------------------------------
-# Never execute notebooks during the Sphinx build.  Outputs must be pre-stored
-# in the .ipynb files (run `pixi run nb-execute` to generate them locally or
-# in CI before the docs build).
-nb_execution_mode = "off"
-
+# Templates and static files
 templates_path = ["_templates"]
 source_suffix = [".rst", ".md", ".ipynb"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Intersphinx mapping
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "xarray": ("https://docs.xarray.dev/en/stable/", None),
     "networkx": ("https://networkx.org/documentation/stable/", None),
+    "nanodisort": ("https://nanodisort.readthedocs.io/stable/", None),
 }
 
+# Autodoc settings
 autodoc_default_options = {
     "members": True,
     "inherited-members": True,
@@ -51,9 +51,14 @@ autodoc_default_options = {
     "show-inheritance": True,
 }
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# MyST-NB options
+# Never execute notebooks during the Sphinx build.  Outputs must be pre-stored
+# in the .ipynb files (run `pixi run nb-execute` to generate them locally or
+# in CI before the docs build).
+nb_execution_mode = "off"
 
+# HTML output options
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_title = "eradiate-disort"
@@ -65,6 +70,9 @@ html_theme_options = {
     "accent_color": "blue",
     "navigation_with_keys": True,
     "github_url": "https://github.com/eradiate/eradiate-disort",
+    "light_logo": "_static/eradiate-disort-logo-typo_simple-black.svg",
+    "dark_logo": "_static/eradiate-disort-logo-typo_simple-white.svg",
 }
 
+# Pygments options
 pygments_style = "default"
