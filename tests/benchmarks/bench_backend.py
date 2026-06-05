1ba8fb7bfdf359c049cd7b6745fe01948eface0a
Vincent Leroy <vincent.leroy@rayference.eu>
2026-06-05 15:04:11 +0200

    bench: Add mitsuba backend tests, add benchmark groups
    Those benchmarks are now correctly grouped when displayed, which makes
    comparisons more meaningful (in particular the "multiplier" metrics
    which now show the difference between the DISORT and Mitsuba runs of
    the same case). This is still WIP, the benchmarking cases still need
    fine-tuning.

--- tests/benchmarks/bench_backend.py
+++ tests/benchmarks/bench_backend.py
@@ -2,35 +2,84 @@
 DISORT backend performance benchmarks.
 """
 
+import eradiate
 import pytest
 
 import eradiate_disort as ed
 from eradiate_disort.testing import cases
 
 
-@pytest.mark.benchmark
-class BenchDisortMonoMode:
-    def bench_no_atmo(self, benchmark, mode_mono):
-        benchmark(ed.DisortBackend().run, cases.no_atmo(sza=30.0, backend="disort"))
+class BenchMonoNoAtmo:
+    @pytest.mark.benchmark(group="no_atmo")
+    def bench_no_atmo_disort(self, benchmark, mode_mono):
+        exp = cases.no_atmo(sza=30.0, backend="disort")
+        backend = ed.DisortBackend()
+        benchmark(backend.run, exp)
 
-    @pytest.mark.parametrize("phase", ["isotropic", "rayleigh"])
-    def bench_single_layer(self, benchmark, mode_mono, phase):
-        benchmark(
-            ed.DisortBackend().run,
-            cases.single_layer(sza=30.0, phase=phase, backend="disort"),
-        )
-
-    def bench_two_layers(self, benchmark, mode_mono):
-        benchmark(ed.DisortBackend().run, cases.two_layers(sza=30.0, backend="disort"))
+    @pytest.mark.benchmark(group="no_atmo")
+    def bench_no_atmo_mitsuba(self, benchmark, mode_mono):
+        exp = cases.no_atmo(sza=30.0, backend="mitsuba")
+        benchmark(eradiate.run, exp)
 
 
-@pytest.mark.benchmark
-class BenchDisortCkdMode:
-    def bench_molecular(self, benchmark, mode_ckd):
-        benchmark(ed.DisortBackend().run, cases.molecular(sza=30.0, backend="disort"))
+@pytest.mark.parametrize("phase", ["isotropic", "rayleigh"])
+class BenchMonoSingleLayer:
+    @pytest.mark.benchmark(group="single_layer")
+    def bench_single_layer_disort(self, benchmark, mode_mono, phase):
+        backend = ed.DisortBackend()
+        exp = cases.single_layer(sza=30.0, phase=phase, backend="disort")
+        benchmark(backend.run, exp)
 
-    def bench_aerosols(self, benchmark, mode_ckd):
-        benchmark(ed.DisortBackend().run, cases.aerosols(sza=30.0, backend="disort"))
+    @pytest.mark.benchmark(group="single_layer")
+    def bench_single_layer_mitsuba(self, benchmark, mode_mono, phase):
+        exp = cases.single_layer(sza=30.0, phase=phase, backend="mitsuba")
+        benchmark(eradiate.run, exp)
 
-    def bench_full_atmo(self, benchmark, mode_ckd):
-        benchmark(ed.DisortBackend().run, cases.full_atmo(sza=30.0, backend="disort"))
+
+class BenchMonoTwoLayers:
+    @pytest.mark.benchmark(group="two_layers")
+    def bench_two_layers_disort(self, benchmark, mode_mono):
+        backend = ed.DisortBackend()
+        exp = cases.two_layers(sza=30.0, backend="disort")
+        benchmark(backend.run, exp)
+
+    @pytest.mark.benchmark(group="two_layers")
+    def bench_two_layers_mitsuba(self, benchmark, mode_mono):
+        exp = cases.two_layers(sza=30.0, backend="mitsuba")
+        benchmark(eradiate.run, exp)
+
+
+@pytest.mark.benchmark(group="molecular")
+class BenchCkdMolecular:
+    def bench_molecular_disort(self, benchmark, mode_ckd):
+        backend = ed.DisortBackend()
+        exp = cases.molecular(sza=30.0, backend="disort")
+        benchmark(backend.run, exp)
+
+    def bench_molecular_mitsuba(self, benchmark, mode_ckd):
+        exp = cases.molecular(sza=30.0, backend="mitsuba")
+        benchmark(eradiate.run, exp)
+
+
+@pytest.mark.benchmark(group="aerosols")
+class BenchCkdAerosols:
+    def bench_aerosols_disort(self, benchmark, mode_ckd):
+        backend = ed.DisortBackend()
+        exp = cases.aerosols(sza=30.0, backend="disort")
+        benchmark(backend.run, exp)
+
+    def bench_aerosols_mitsuba(self, benchmark, mode_ckd):
+        exp = cases.aerosols(sza=30.0, backend="mitsuba")
+        benchmark(eradiate.run, exp)
+
+
+@pytest.mark.benchmark(group="full")
+class BenchCkdFull:
+    def bench_full_atmo_disort(self, benchmark, mode_ckd):
+        backend = ed.DisortBackend()
+        exp = cases.full_atmo(sza=30.0, backend="disort")
+        benchmark(backend.run, exp)
+
+    def bench_bench_full_atmo_mitsuba(self, benchmark, mode_ckd):
+        exp = cases.full_atmo(sza=30.0, backend="mitsuba")
+        benchmark(eradiate.run, exp)
